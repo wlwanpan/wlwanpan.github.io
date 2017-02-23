30a00f28056a0f28c65ad89238682ffaaa68fec1
wlwannpan <wlwanpan@uwaterloo.ca>
2017-02-22 16:47:24 -0800

    added cat-logo

--- js/animation.js
+++ js/animation.js
@@ -1,38 +1,91 @@
 
+var setup = function() {
+
+    var cat = Snap("#c1-c7");
+    var logo = Snap("#l1-l7");
+
+    var catPath = cat.selectAll('path');
+    var logoPath = logo.selectAll('path');
+    var scatterDuration = 800;
+    var fixationDuration = 700;
+
+    $('.to-logo').click(function() {
+
+        console.log("d d," * 3);
+
+        var logoContainer = $("#logo-container");
+        var svgContainer = $("#logo-svg-box")[0];
+
+        logoContainer.css('min-height', '50px');
+        logoContainer.css('min-width', '50px');
+        logoContainer.css('z-index', '9999');
+        
+        // First Scatter Animation
+        scatter(svgContainer);
+        logoContainer.velocity({
+            width: '35%', 
+            height: '35%',
+        }, {
+            easing: "easeIn",
+            duration: scatterDuration,
+        });
+        // Second Queue Morph+fix Logo
+        setTimeout(function() {
+            transmutate(svgContainer);
+            logoContainer.velocity({
+                top: "0px",
+                left: "5.5%",
+                height: '50px',
+                width: '50px',
+            }, {
+                duration: fixationDuration,
+                easing: "easeInQuad",
+                complete: function () {
+                    svgContainer.setAttribute('viewBox', "0 0 77.7 77");
+                    $("#head").removeClass('head-animate');
+                    $(this).css('position', 'fixed');
+                }
+            });
+        }, scatterDuration);
+    });
+    var scatter = function (box) {
+        box.setAttribute('viewBox', "0 0 100 100");
+        for (var i = 0; i < 7; i++) {
+            var angle = (Math.random() * (90 + 90) - 90);
+            catPath[i].animate({transform: "t 0 0, r" + angle + "50% 50%," + angle + "50% 50%, 0" }, 
+                                scatterDuration, mina.easeInOut);
+        }
+    }
+    var transmutate = function (box) {
+        
+        for (var i = 0; i < 7; i++) {
+            catPath[i].animate({transform: "t 0, 0", d: logoPath[i].attr("d")}, fixationDuration, mina.easeIn);
+        }
+    }
+}
 $(window).on('load', function() {
 
     var HeaderHeight = 50;
 	
 	$('#intro-container').css('height', window.innerHeight-HeaderHeight);
 
+    setup();
+
 	$('.tlt').textillate({
-		in: {effect: 'bounceInDown'}
+        initialDelay: 0,
+        in: {effect: 'bounceInDown'}
 	});
     //introduction phrases 
 	$(function(){
         $(".command-typing").typed({
-            strings: ["Welcome ^200 to my personal website.","I like to ^500 animate stuff ^200 on the web.", "And YES, ^200 this is how slow I usually type.", "I am still working on the website ...", " Thanks for dropping by."],
-            typeSpeed: 50,
-            startDelay: 2000,
-            backSpeed: 10 ,
+            strings: ["Welcome ^200 to my personal website.", "I like to ^500 animate stuff ^200 on the web.", "And if you don't like the cat :(", " Hit its head ..."],
+            typeSpeed: 40,
+            startDelay: 1200,
+            backSpeed: 5 ,
             backDelay: 2000,
             showCursor: false,
             contentType: 'html',
         });
-    });
-
-    document.addEventListener('keydown', function(event) {
-
-        //event.preventDefault()
-        //right pressed
-        if(event.keyCode == 39) {
-            //$('#lego-right-arm').addClass('lego-move');
-            //$('#lego-left-arm').addClass('lego-move');
-        }
-        else {
-            //$('#lego-right-arm').removeClass('lego-move');
-        }
-
     });
 
     // Toggle to Minimize or Maximize All Resume Brackets
@@ -85,14 +138,16 @@ $(window).on('load', function() {
         var ScrollPosn = $(this).scrollTop();
         var SectionAboutme = $("#section-aboutme").offset().top - HeaderHeight;
         var SectionProject = $("#section-project").offset().top - HeaderHeight;
+        var buffer = window.innerHeight/2;
         // section about me
-        if (ScrollPosn > SectionAboutme/2) {
-        	$(".section-wrapper h1").css('opacity', 1);
-        } else {
-        	$(".section-wrapper h1").css('opacity', 0);
-        }
+        if (ScrollPosn > SectionAboutme-buffer) {$("#aboutme-container h1").css('opacity', 1);} 
+        else {$("#aboutme-container h1").css('opacity', 0);}
+        if (ScrollPosn > SectionProject-buffer) {$("#section-project h1").css('opacity', 1);}
+        else {$("#section-project h1").css('opacity', 0);}
 
         if (ScrollPosn >= SectionAboutme && ScrollPosn < SectionProject) {
+
+            //$('#logo-svg-box g').css('fill', '#ffffff !important;');
             $("#icon-container a").css('color', 'white');
             $("#header").css('background', '#191919');
             $("#icon-container").css('border-bottom', '2px solid white');
@@ -122,21 +177,12 @@ $(window).on('load', function() {
                 
                 var word = Phrase[i];
 
-                if (white["words"].includes(word)) {
-                    var color = white["color"];
-                    console.log(color);
-                    if (word == 'and' || word == '..') {ColorizedPhrase += " " + word.fontcolor(color) + " ";}
-                    else if (word == '{' || word == '(') {ColorizedPhrase += " " + word.fontcolor(color);}
-                    else {ColorizedPhrase += word.fontcolor(color) + " ";}
-                }
-
+                if (word == '_') {ColorizedPhrase += " ";}
+                else if (white["words"].includes(word)) {ColorizedPhrase += word.fontcolor(white["color"]);}
                 else if (brown["words"].includes(word)) {ColorizedPhrase += word.fontcolor(brown["color"]);}
-
-                else if (green["words"].includes(word)) {ColorizedPhrase += " " + word.fontcolor(green["color"]);} 
-
-                else if (red["words"].includes(word)) {ColorizedPhrase += " " + word.fontcolor(red["color"]);}
-
-                else {ColorizedPhrase += " " + word.fontcolor("#5CD9EF");}
+                else if (green["words"].includes(word)) {ColorizedPhrase += word.fontcolor(green["color"]);} 
+                else if (red["words"].includes(word)) {ColorizedPhrase += word.fontcolor(red["color"]);}
+                else {ColorizedPhrase += word.fontcolor("#5CD9EF");}
 
             }
 
@@ -152,15 +198,20 @@ $(window).on('load', function() {
         e.preventDefault();
         e.stopPropagation();
 
-        if (!($(document).scrollTop() == $(section).offset().top - HeaderHeight)) {
-
+        if ($(document).scrollTop() == $(section).offset().top - HeaderHeight) {
+            //$(this).css('color', '#cc0000');
+        } else {
             $('html, body').velocity('scroll', {
                 duration: 800,
                 offset: $(section).offset().top - (HeaderHeight*2),
                 easing: 'ease-in-out'
             });
         }
-
     });
+});
+$(window).resize(function() {
+
+    var HeaderHeight = 50;
+    $('#intro-container').css('height', window.innerHeight-HeaderHeight);
 
 });
